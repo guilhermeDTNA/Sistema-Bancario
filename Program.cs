fa79acb907de883ed58926412c894b9f0a415341
Guilherme Rocha <gleite98@gmail.com>
2021-05-27 15:18:35 -0300

    Estilizando menu e tratando o valores digitados

--- Program.cs
+++ Program.cs
@@ -40,28 +40,77 @@ namespace DIO.bank
                         break;
 
                     default:
-                        throw new ArgumentOutOfRangeException();
+                        Console.WriteLine("Opção inválida, tente novamente");
+                        break;
                 }
                 opcao = menu();
             }
 
             Console.WriteLine("Obrigado por usar nossos serviços!");
-            Console.ReadLine();
 
         }
 
         private static void transferir()
         {
-           Console.Write("Digite o número da conta de origem: ");
-           int indiceContaOrigem = int.Parse(Console.ReadLine());
+            Console.Write("Digite o número da conta de origem: ");
+            int indiceContaOrigem = int.Parse(Console.ReadLine());
 
-           Console.Write("Digite o número da conta de destino: ");
-           int indiceContaDestino = int.Parse(Console.ReadLine());
+            if (!verificaConta(indiceContaOrigem))
+            {
+                Console.WriteLine("Conta inexistente.");
 
-           Console.Write("Digite o valor a ser transferido: ");
-           double valorTransferencia = double.Parse(Console.ReadLine());
+                return;
+            }
 
-           listaContas[indiceContaOrigem].Transferir(valorTransferencia, listaContas[indiceContaDestino]);
+            Console.Write("Digite o número da conta de destino: ");
+            int indiceContaDestino = int.Parse(Console.ReadLine());
+
+            if (indiceContaOrigem == indiceContaDestino)
+            {
+                Console.WriteLine("As contas são iguais, tente novamente");
+
+                return;
+            }
+
+            if (!verificaConta(indiceContaDestino))
+            {
+                Console.WriteLine("Conta inexistente.");
+
+                return;
+            }
+
+            Console.Write("Digite o valor a ser transferido: ");
+            double valorTransferencia = double.Parse(Console.ReadLine().Replace(".", ","));
+
+            if (valorTransferencia <= 0)
+            {
+                Console.WriteLine("Valor inválido!");
+
+                return;
+            }
+
+            try
+            {
+                listaContas[indiceContaOrigem].Transferir(valorTransferencia, listaContas[indiceContaDestino]);
+
+                Console.WriteLine("Tranferência realizada com sucesso!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Não foi possível realizar a transação.");
+                Console.WriteLine($"Erro: {e}");
+            }
+        }
+
+        private static bool verificaConta(int indice) //Verifica se a conta existe
+        {
+            //Verifica se a conta existe
+            if (indice < 0 || indice > listaContas.Count)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         private static void depositar()
@@ -69,10 +118,34 @@ namespace DIO.bank
             Console.WriteLine("Digite o número da conta: ");
             int indiceConta = int.Parse(Console.ReadLine());
 
-            Console.Write("Digite o valor a ser depositado: ");
-            double valorDeposito = double.Parse(Console.ReadLine());
+            if (!verificaConta(indiceConta))
+            {
+                Console.WriteLine("Conta inexistente");
 
-            listaContas[indiceConta].Depositar(valorDeposito);
+                return;
+            }
+
+            Console.Write("Digite o valor a ser depositado: ");
+            double valorDeposito = double.Parse(Console.ReadLine().Replace(".", ","));
+
+            if (valorDeposito <= 0)
+            {
+                Console.WriteLine("Valor inválido!");
+
+                return;
+            }
+
+            try
+            {
+                listaContas[indiceConta].Depositar(valorDeposito);
+
+                Console.WriteLine("Depósito efetuado com sucesso!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Não foi possível efetuar o depósito");
+                Console.WriteLine($"Erro: {e}");
+            }
         }
 
         private static void sacar()
@@ -80,22 +153,48 @@ namespace DIO.bank
             Console.WriteLine("Digite o número da conta: ");
             int indiceConta = int.Parse(Console.ReadLine());
 
-            Console.Write("Digite o valor a ser sacado: ");
-            double valorSaque = double.Parse(Console.ReadLine());
+            if (!verificaConta(indiceConta))
+            {
+                Console.WriteLine("Conta inexistente");
 
-            listaContas[indiceConta].Sacar(valorSaque);
+                return;
+            }
+
+            Console.Write("Digite o valor a ser sacado: ");
+            double valorSaque = double.Parse(Console.ReadLine().Replace(".", ","));
+
+            if (valorSaque <= 0)
+            {
+                Console.WriteLine("Valor inválido!");
+
+                return;
+            }
+
+            try
+            {
+                listaContas[indiceConta].Sacar(valorSaque);
+
+                Console.WriteLine("Saque efetuado com sucesso!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Não foi possível efetuar o saque");
+                Console.WriteLine($"Erro: {e}");
+            }
         }
 
         private static void listarContas()
         {
             Console.WriteLine("Lista contas");
 
-            if(listaContas.Count == 0){
+            if (listaContas.Count == 0)
+            {
                 Console.WriteLine("Nenhuma conta cadastrada!");
 
                 return;
             }
-            for(int i=0; i<listaContas.Count; i++){
+            for (int i = 0; i < listaContas.Count; i++)
+            {
                 Conta conta = listaContas[i];
 
                 Console.Write("#{0} - ", i);
@@ -111,28 +210,56 @@ namespace DIO.bank
             Console.Write("Digite 1 para Conta Física ou 2 para Conta Jurídica: ");
             int entradaTipoConta = int.Parse(Console.ReadLine());
 
+            //Verifica se o tipo de pessoa existe
+            if (entradaTipoConta != 1 && entradaTipoConta != 2)
+            {
+                Console.WriteLine("Valor inexistente, digite novamente");
+
+                return;
+            }
+
             Console.Write("Digite o nome do cliente: ");
             string entradaNome = Console.ReadLine();
 
-            Console.Write("Digite o saldo inicial:");
-            double entradaSaldo = double.Parse(Console.ReadLine());
+            Console.Write("Digite o saldo inicial: ");
+            double entradaSaldo = double.Parse(Console.ReadLine().Replace(".", ","));
 
             Console.Write("Digite o crédito: ");
-            double entradaCredito = double.Parse(Console.ReadLine());
+            double entradaCredito = double.Parse(Console.ReadLine().Replace(".", ","));
 
-            Conta conta = new Conta(tipoConta: (TipoConta) entradaTipoConta, 
-                                                        saldo: entradaSaldo, 
-                                                        credito: entradaCredito, 
-                                                        nome: entradaNome);
-            
-            listaContas.Add(conta);
+            if (string.IsNullOrEmpty(entradaNome) || double.IsNaN(entradaSaldo) || double.IsNaN(entradaCredito))
+            {
+                Console.WriteLine("Um ou mais valores inválidos, tente novamente");
+
+                return;
+            }
+
+            try
+            {
+                Conta conta = new Conta(tipoConta: (TipoConta)entradaTipoConta,
+                                                            saldo: entradaSaldo,
+                                                            credito: entradaCredito,
+                                                            nome: entradaNome);
+
+                listaContas.Add(conta);
+
+                Console.WriteLine("Conta cadastrada com sucesso!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Não foi possível adicionar");
+                Console.WriteLine($"Erro: {e}");
+            }
         }
 
         public static string menu()
         {
             Console.WriteLine();
-            Console.WriteLine("DIO Bank soluções");
-            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("|--------------------------------------|");
+            Console.WriteLine("|            Bem-Vindo!                |");
+            Console.WriteLine("|  Informe a opção desejada:           |");
+            Console.WriteLine("|--------------------------------------|");
+            Console.WriteLine();
 
             Console.WriteLine("1 - Listar contas");
             Console.WriteLine("2 - Inserir nova conta");
